5e35dc21353226a87549d96da6d1db0a514d6054
Zemlaman <matej.zemla@seznam.cz>
2021-01-04 11:01:00 +0100

    lepší trubka 2.0

--- AngularWeb/src/app/my-pipe.pipe.ts
+++ AngularWeb/src/app/my-pipe.pipe.ts
@@ -7,7 +7,7 @@ export class MyPipePipe implements PipeTransform {
 
   transform(value: string, newEnd: string): string {
         let upgusr = value;
-        upgusr = upgusr.slice(0, - newEnd.length);
+        upgusr = upgusr.slice(0, value.length - newEnd.length);
         upgusr = upgusr + newEnd;
         return upgusr;
   }
